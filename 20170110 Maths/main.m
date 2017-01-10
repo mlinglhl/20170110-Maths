e79db7b1c45b44f9684c17a5adbecd14fb16802d
mlinglhl <minhung.ling@gmail.com>
2017-01-10 11:16:45 -0500

    Initial Commit

---
+++ 20170110 Maths/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  20170110 Maths
+//
+//  Created by Minhung Ling on 2017-01-10.
+//  Copyright © 2017 Minhung Ling. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
